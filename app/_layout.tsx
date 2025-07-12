739a202462c9604c53e668d4e8ea6d130a863432
23bimal23 <bimal23paudel777@gmail.com>
2025-07-12 22:24:05 +0545

    added fonts and imported to layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,27 @@
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+
 import '../globals.css';
+
+
+
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded,error]  = useFonts({
+    "OuickSand-Bold": require("../assets/fonts/Quicksand-Bold.ttf"),
+    "OuickSand-Medium": require("../assets/fonts/Quicksand-Medium.ttf"),
+    "OuickSand-Regular": require("../assets/fonts/Quicksand-Regular.ttf"),
+    "OuickSand-SemiBold": require("../assets/fonts/Quicksand-SemiBold.ttf"),
+    "QuickSand-Light": require("../assets/fonts/Quicksand-Light.ttf"),
+
+  })
+  useEffect(() => {
+    if (error) {
+      console.warn("Error loading fonts:", error);
+    }
+    if(fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded,error]);
+
+
+  return <Stack screenOptions={{headerShown:false}} />;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,10 @@
 import { Text, View } from "react-native";
 
+
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-blue-500">
-      <Text className="text-white text-4xl">Hello, Tailwind!</Text>
+    <View className="flex-1 items-center justify-center bg-white-500">
+      <Text className="text-primary text-5xl text-center font-quicksand-bold">Welcome to my Fast food app!</Text>
     </View>
   );
 }
