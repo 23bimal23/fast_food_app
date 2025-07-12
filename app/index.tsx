0b2b2187b771621a0a086999db63a4c042f156ab
23bimal23 <bimal23paudel777@gmail.com>
2025-07-12 22:05:55 +0545

    initialized project

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+
+  return (
+
+    <View className="flex-1 items-center justify-center bg-blue-500">
+
+      <Text className="text-white text-4xl">Hello, Tailwind!</Text>
+
+    </View>);
+}
